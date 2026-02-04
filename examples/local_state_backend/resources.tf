0a17b4499ef366de8ec4452dea6a8cd9bcd53c79
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-04 17:25:41 +0300

    initial examples

---
+++ examples/local_state_backend/resources.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "cluster" {
+  triggers = {
+    password = random_password.pass.result
+  }
+
+}
+
+resource "random_password" "pass" {
+  length           = 32
+  special          = false
+}
